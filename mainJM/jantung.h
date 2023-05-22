cb6e24d621406c8aee52bc3af3db8b99e3af6ade
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-05-22 20:13:29 +0700

    untuk mengimplementasi tambahDokter(dokter*pDokter) tambahPasien(pasien*pPasien) dan  dalam kelas dokter dan untuk mencetak  daftar dokter dan pasien ke dalam vektor daftar_dokter dan daftar_pasien

--- mainJM/jantung.h
+++ mainJM/jantung.h
@@ -1,1 +1,15 @@
-#pragma once
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+using namespace std;
+
+class jantung {
+public:
+	jantung() {
+		cout << "jangan dihidupkan\n";
+	}
+	~jantung() {
+		cout << "jangan dihmatikan\n";
+	}
+};
+#endif
